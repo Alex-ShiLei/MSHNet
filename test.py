4caec4fb996ec66c0586f833816307facdb4e6f5
Alex-ShiLei <2457072341@qq.com>
2022-03-21 22:53:58 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,97 @@
+r""" Hypercorrelation Squeeze testing code """
+import argparse
+
+import torch.nn.functional as F
+import torch.nn as nn
+import torch
+
+from model.mshnet import MsimilarityHyperrelationNetwork
+from common.logger import Logger, AverageMeter
+from common.vis import Visualizer
+from common.evaluation import Evaluator
+from common import utils
+from data.dataset import FSSDataset
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+def test(model, dataloader, nshot):
+    r""" Test HSNet """
+
+    # Freeze randomness during testing for reproducibility
+    utils.fix_randseed(0)
+    average_meter = AverageMeter(dataloader.dataset)
+
+    for idx, batch in enumerate(dataloader):
+
+        # 1. Hypercorrelation Squeeze Networks forward pass
+        batch = utils.to_cuda(batch)
+        pred_mask = model.predict_mask_nshot(batch, nshot=nshot)
+
+        assert pred_mask.size() == batch['query_mask'].size()
+
+        # 2. Evaluate prediction
+        area_inter, area_union = Evaluator.classify_prediction(pred_mask.clone(), batch)
+        average_meter.update(area_inter, area_union, batch['class_id'], loss=None)
+        average_meter.write_process(idx, len(dataloader), epoch=-1, write_batch_idx=1)
+
+        # Visualize predictions
+        if Visualizer.visualize:
+            Visualizer.visualize_prediction_batch(batch['support_imgs'], batch['support_masks'],
+                                                  batch['query_img'], batch['query_mask'],
+                                                  pred_mask, batch['class_id'], idx,
+                                                  area_inter[1].float() / area_union[1].float())
+
+    # Write evaluation results
+    average_meter.write_result('Test', 0)
+    miou, fb_iou = average_meter.compute_iou()
+
+    return miou, fb_iou
+
+
+if __name__ == '__main__':
+
+    # Arguments parsing
+    parser = argparse.ArgumentParser(description='Hypercorrelation Squeeze Pytorch Implementation')
+    parser.add_argument('--datapath', type=str, default='/home/alex/pytorch/data/VOCdevkit')
+    #parser.add_argument('--datapath', type=str, default='/home/alex/pytorch/data')
+    parser.add_argument('--benchmark', type=str, default='pascal', choices=['pascal', 'coco', 'fss'])
+    parser.add_argument('--logpath', type=str, default='')
+    parser.add_argument('--bsz', type=int, default=1)
+    parser.add_argument('--nworker', type=int, default=1)
+    parser.add_argument('--load', type=str, default='./resume_pascal50_0.pth')
+    parser.add_argument('--fold', type=int, default=0, choices=[0, 1, 2, 3])
+    parser.add_argument('--nshot', type=int, default=1)
+    parser.add_argument('--backbone', type=str, default='resnet50', choices=['vgg16', 'resnet50', 'resnet101'])
+    parser.add_argument('--visualize', action='store_true')
+    parser.add_argument('--use_original_imgsize', action='store_true')
+    args = parser.parse_args()
+    Logger.initialize(args, training=False)
+
+    # Model initialization
+    model = MsimilarityHyperrelationNetwork(args.backbone, args.use_original_imgsize,shot=args.nshot)
+    model.eval()
+    Logger.log_params(model)
+
+    # Device setup
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    Logger.info('# available GPUs: %d' % torch.cuda.device_count())
+    model = model.cuda()
+    #model.to(device)
+
+    # Load trained model
+    if args.load == '': raise Exception('Pretrained model not specified.')
+    model.load_state_dict(torch.load(args.load))
+
+    # Helper classes (for testing) initialization
+    Evaluator.initialize()
+    Visualizer.initialize(args.visualize)
+
+    # Dataset initialization
+    FSSDataset.initialize(img_size=473, datapath=args.datapath, use_original_imgsize=args.use_original_imgsize)
+    dataloader_test = FSSDataset.build_dataloader(args.benchmark, args.bsz, args.nworker, args.fold, 'test', args.nshot)
+
+    # Test HSNet
+    with torch.no_grad():
+        test_miou, test_fb_iou = test(model, dataloader_test, args.nshot)
+    Logger.info('Fold %d mIoU: %5.2f \t FB-IoU: %5.2f' % (args.fold, test_miou.item(), test_fb_iou.item()))
+    Logger.info('==================== Finished Testing ====================')
